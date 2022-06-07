c9371bee2a7939ad9659ba4e9d6d7a728143c383
Cleb3rbatista <101151617+Cleb3rbatista@users.noreply.github.com>
2022-06-06 22:21:21 -0300

    terminando

--- jogo.py
+++ jogo.py
@@ -1,11 +1,17 @@
 import random
-from tkinter import N
 def play():
-    user=input("Qual e sua escolha? 'R' para pedra, 'P' para papel e 'T' para tesoura"/N)
-    computer=random.choice(['r''p''t'])
-        if user==computer:
-            return 'tie'
-        if ()
+    user=input("Qual e sua escolha? 'R' para pedra, 'P' para papel e 'T' para tesoura\n")
+    computer = random.choice(['r','p','t'])
+    
+    if user == computer:
+        return "empate"
+        
+    if is_win (user,computer):
+        return "Parabens, você venceu!"
+    
+    return"lamento, você perdeu!"
+        
 def is_win(jogador,computer_oponente):
     if(jogador=='r' and computer_oponente=='t') or (jogador=='p' and computer_oponente=='r') or (jogador=='t' and computer_oponente=='p'):
         return True
+print (play())
