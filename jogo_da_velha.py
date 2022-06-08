02c6f8699f569277c0bc3f2fc6fb4170393ed3bd
Cleb3rbatista <101151617+Cleb3rbatista@users.noreply.github.com>
2022-06-07 21:51:18 -0300

    por hj

---
+++ jogo_da_velha.py
@@ -0,0 +1,13 @@
+import math
+import random
+ 
+class jogador:
+    def iniciar(self,letter):
+        self.letter=letter
+    def get_move(self,game):
+        pass
+class computer_oponente(jogador):
+    def iniciar(self,letter):
+        super(computer_oponente,self).iniciar
+        
+     
