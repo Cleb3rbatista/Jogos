4a26a5b15b47f4df03169bbe1cc017eb3fe0d7fc
Cleb3rbatista <101151617+Cleb3rbatista@users.noreply.github.com>
2022-11-19 19:21:10 -0300

    create Adivinhe_a_Palavra.py

---
+++ Adivinhe_a_Palavra.py
@@ -0,0 +1,51 @@
+import random
+def jogo_acerta_palavra():
+    mylist = ["amarelo", "banana", "piso", "precioso", "hunched" , "dado", \
+              "Rabino", "agudo", "antro", "anunciar", "bebida", "enfermeira",\
+              "urso", "chefe", "sangrar", "ombros", "barco", "senhora", "escrita"]
+    palavra_aleatoria=random.choice(mylist)
+    palavra_secreta = palavra_aleatoria.upper()
+    letras_acertadas = ''
+    entrar_sair = input("Jogo de acerta a palavra secreta [E]ntrar e [S]air\n").upper()
+    contador_tentativas = 0
+    if entrar_sair == 'E':
+        while contador_tentativas >= 0:
+            letra_dig= input("Digite uma letra: ").upper()
+            if len(letra_dig) > 1:
+                print("Digite apenas uma letra")
+                contador_tentativas += 1
+                continue
+            if letras_acertadas.count(letra_dig) != 0:
+                print("Voce ja digitou essa letra antes")
+                contador_tentativas += 1
+                continue
+            if palavra_secreta.count(letra_dig) != 0:
+                print("A letra pertence a palavra secreta")
+                contador_tentativas += 1
+                if letra_dig in palavra_secreta:
+                    letras_acertadas += letra_dig  
+                palavra_formada = ''            
+                for letras_secreta in palavra_secreta:
+                    if letras_secreta in letras_acertadas:
+                        palavra_formada +=letras_secreta
+                    else:
+                        palavra_formada += "*"   
+                print(palavra_formada)
+                if palavra_formada == palavra_secreta:
+                    print("PARABENS!!, VOCE GANHOU" 
+                        " voce ganhou em " , contador_tentativas , "tentativas")
+                    jogar_novamente = input("Deseja jogar novamente ? [S]im ou [N]ão?\n ").upper()
+                    if jogar_novamente == "S":
+                        jogo_acerta_palavra()
+                    elif jogar_novamente == "N":
+                        print("Saindo...")
+                        break
+            elif palavra_secreta.count(letra_dig) == 0:
+                print(" A letra digitada não pertence a palavra secreta")
+                contador_tentativas += 1           
+    elif entrar_sair == "S":
+        print ("Saindo...");
+    else:
+        print("Valor digitado invalido")
+        
+jogo_acerta_palavra()
